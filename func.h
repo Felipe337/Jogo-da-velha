818ffb76571de99d3985226132e0bfe59a873990
Felipe337 <116205272+Felipe337@users.noreply.github.com>
2024-08-16 12:53:45 -0400

    Create func.h
    Create func.h

---
+++ func.h
@@ -0,0 +1,4 @@
+void inicializarTabuleiro(char tabuleiro[3][3]);
+void exibirTabuleiro(char tabuleiro[3][3]);
+char verificarVencedor(char tabuleiro[3][3]);
+void alternarJogador(char *jogadorAtual);
